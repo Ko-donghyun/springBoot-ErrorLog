75fbeb06c1c3622b9e8ddbfd97f958503adfccdb
kodong <kodonghyun1209@gmail.com>
2016-10-28 17:36:34 +0900

    메인 어플리케이션 파일 추가
    - 프로젝트 시작 시 진입점

---
+++ src/main/java/com/example/ErrorLogTestApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorLogTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorLogTestApplication.class, args);
+	}
+}
