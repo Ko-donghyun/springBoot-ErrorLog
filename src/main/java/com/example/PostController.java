ec37294628caedef1bac133b69b412c856917732
kodong <kodonghyun1209@gmail.com>
2016-10-31 15:46:32 +0900

    post 컨트롤러에 대한 처리
    post 요청을 받았을 때
    외부 Rest API 서버에 요청을 보내고
    응답 받은 것을 이용하여 처리하도록 수적

--- src/main/java/com/example/PostController.java
+++ src/main/java/com/example/PostController.java
@@ -1,11 +1,13 @@
 package com.example;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @EnableAutoConfiguration
@@ -18,7 +20,17 @@ public class PostController {
 
   @RequestMapping(value = "/posts", method = RequestMethod.POST)
   public String createPost(@ModelAttribute Post post, Model model) {
-    model.addAttribute("post", post);
+//    model.addAttribute("post", post);
+
+    RestTemplate restTemplate = new RestTemplate();                            // 내부적으로 새로운 서버에 REST API 요청을 하기 위한 Rest Template 도구
+    restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+
+    String url = "https://rest-api-ko-donghyun.c9users.io/posts.json";         // 새로운 서버의 URL 변경
+    Post postObj = restTemplate.postForObject(url, post, Post.class);          // 새로운 서버의 JSON 결과를 POJO로 매핑
+    model.addAttribute("post", postObj);                                       // View 업데이트를 위한 Model에 POJO 객체 저장
+
+    System.out.println(postObj.getTitle());
+    System.out.println(postObj.getContent());
 
     return "show";
   }
