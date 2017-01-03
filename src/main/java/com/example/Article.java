34f34f66cd14eabfd16e879e77a935a5d2a0444d
kodong <kodonghyun1209@gmail.com>
2017-01-03 16:59:33 +0900

    계층 구조의 YAML 파일을 POJO로 매핑하기
     - Article 안에 Comment를 리스트로 가지고 있는 구조를 위해 클래스 작성

---
+++ src/main/java/com/example/Article.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Article implements Serializable {
+  private long id;
+  private String title;
+  private String content;
+  private List<Comment> comments;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public List<Comment> getComments() {
+    return comments;
+  }
+
+  public void setComments(List<Comment> comments) {
+    this.comments = comments;
+  }
+}
