8c79407fec73789b2e71fa75169af7c22d360887
kodong <kodonghyun1209@gmail.com>
2017-01-09 14:05:37 +0900

    Article 클래스에 파일을 받을 수 있도록 변수 추가

--- src/main/java/com/example/Article.java
+++ src/main/java/com/example/Article.java
@@ -1,6 +1,8 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
 import java.util.List;
@@ -10,6 +12,26 @@ public class Article implements Serializable {
   private String title;
   private String content;
   private List<Comment> comments;
+
+  @JsonIgnore
+  private MultipartFile file;
+  // 파일 명을 받고 싶을면 아래의 주석을 제거, 아래와 같이 file이라는 결과를 받을 수 있다.
+  // {"id":0,"title":"제목","content":"아아아","comments":null,"file":"겨울 배경화면5.jpg"}
+
+//  @JsonProperty("file")
+//  private String fileName;
+//
+//  public String getFileName() {
+//    return this.file.getOriginalFilename();
+//  }
+
+  public MultipartFile getFile() {
+    return file;
+  }
+
+  public void setFile(MultipartFile file) {
+    this.file = file;
+  }
 
   public long getId() {
     return id;
